96e84d63cdff3d48e215c8347eee10cb7a60c238
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-04-21 20:11:58 +0530

    uncomment code for production code

--- receiver/reciever.c
+++ receiver/reciever.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "statistics.h"
 
-//int main()
-//{
-//	float input1, input2;
-//	for(int i=0; i<50; i++)
-//	{
-//		scanf("%f,%f", &input1, &input2);
-//		calcStatistics(input1, input2, i);
-//	}
-//	
-//	return 0;
-//}
+int main()
+{
+	float input1, input2;
+	for(int i=0; i<50; i++)
+	{
+		scanf("%f,%f", &input1, &input2);
+		calcStatistics(input1, input2, i);
+	}
+	
+	return 0;
+}
