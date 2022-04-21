7fce20246ff164d1238187314b31627dcca5d0c9
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-04-21 20:34:18 +0530

    correct scanf format according to sender

--- receiver/reciever.c
+++ receiver/reciever.c
@@ -8,7 +8,7 @@ int main()
 	
 	for(int i=0; i<50; i++)
 	{
-		scanf("{\"Temperature\":%0.2f,\"SOC\":%0.2f}", &input1, &input2);
+		scanf("\n{\"Temperature\":%f,\"SOC\":%f}", &input1, &input2);
 		result = calcStatistics(input1, input2, i);
 		printStats(result);
 	}
