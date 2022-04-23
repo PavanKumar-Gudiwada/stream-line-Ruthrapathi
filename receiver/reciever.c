cf7c083ac677fd22e548cb71aa2a7595ee979c9a
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-04-23 15:39:20 +0530

    revert the main to correct receiver

--- receiver/reciever.c
+++ receiver/reciever.c
@@ -6,10 +6,10 @@ int main()
 	float input1, input2;
 	struct results_t result;
 	
-	for(int i=0; i<3; i++)
+	for(int i=0; i<50; i++)
 	{
-		//scanf("\n{\"Temperature\":%f,\"SOC\":%f}", &input1, &input2);
-		scanf("%f,%f", &input1, &input2);
+		scanf("\n{\"Temperature\":%f,\"SOC\":%f}", &input1, &input2);
+		
 		result = calcStatistics(input1, input2, i);
 		printStats(result);
 	}
